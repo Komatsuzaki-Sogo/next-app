4965b5209da6293f7d02d8e950507db768b7f075
*qL2xwwfNdp <sogo.komatsuzaki.xx@ambl.co.jp>
2026-03-10 18:44:17 +0900

    update ユーザーページ戻し

--- src/app/(private)/user/page.tsx
+++ src/app/(private)/user/page.tsx
@@ -4,33 +4,13 @@ export const metadata: Metadata = {
   description: 'パスワード管理アプリのユーザー情報ページです。',
 };
 
-import { auth } from '@/auth';
-import { prisma } from '@/lib/prisma';
 import { CommonSection } from '@/components/layouts/CommonSection';
 import { UserInfo } from '@/components/pages/user/index/UserInfo';
 
-export default async function UserPage() {
-  const session = await auth();
-  const userId = session?.user?.id;
-
-  if (!userId) throw new Error('不正なリクエストです');
-
-  const user = await prisma.user.findUnique({
-    where: { id: userId },
-    select: {
-      id: true,
-      profileImage: true,
-      name: true,
-      email: true,
-      createdAt: true,
-      updatedAt: true,
-    },
-  });
-
-  if (!user) throw new Error('ユーザーが存在しません');
+export default function UserPage() {
   return (
     <CommonSection width="narrow">
-      <UserInfo user={user} />
+      <UserInfo />
     </CommonSection>
   );
 }
